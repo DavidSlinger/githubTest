1c90fd66920935112799e83f66b5c0e6120a2a9c
DavidSlinger <DavidSlinger@users.noreply.github.com>
2020-11-05 18:01:39 -0700

    Update Main.java
    Added a third println statemet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,8 @@ public class Main {
         System.out.println("lets test this mofo");
 
         System.out.println("time for test 2");
+
+        System.out.println("Time for test 3");
+
     }
 }
